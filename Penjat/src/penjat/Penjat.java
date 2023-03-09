c8eb61845413fa3bebc974ef9799e87750b6c2b4
David <David@LAPTOP-BLEI720H>
2023-03-09 10:18:10 +0100

    comit inicial

---
+++ Penjat/src/penjat/Penjat.java
@@ -0,0 +1,102 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package penjat;
+
+/**
+ *
+ * @author David
+ */
+public class Penjat {
+
+    /**
+     * @param args the command line arguments
+     */
+public static void main(String[] args) {
+       
+        final char[][] estatPenjatInicial =
+        {
+          {' ',' ',' ',' ','','','','',' ',' ',' ',' '},                                      
+          {' ',' ',' ','|',' ',' ',' ',' ',' ',' ',' ',' '},
+          {' ',' ',' ','|',' ',' ',' ',' ',' ',' ',' ',' '},
+          {' ',' ',' ','|',' ',' ',' ',' ',' ',' ',' ',' '},
+          {' ',' ',' ','|',' ',' ',' ',' ',' ',' ',' ',' '},
+          {' ',' ',' ','|',' ',' ',' ',' ',' ',' ',' ',' '},
+          {' ','','','|','_',' ',' ',' ',' ',' ',' ',' '},                                      
+          {'|',' ',' ',' ',' ','|','','','','','_',' '},
+          {'|',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ','|'},
+          {'|','','','','','','','','','','','|'}
+         }; 
+        final String[] paraules = {"patata","armari","bicicleta",
+                            "advocat","ascensor","astronauta","autopista",
+                            "avinguda","bigoti","carretera","castanya",
+                            "cervell","civada","cultura","dentista","esquena",
+                            "estrella","formatge","gendre","genoll",
+                            "infermera","internet","maduixa","malaltia",
+                            "maluc","mandarina","maquinista","motocicleta",
+                            "nebot","pastanaga","patinet","perruqueria",
+                            "pissarra","professor","quadrat","taronja",
+                            "tramvia","trapezi","tricicle","violeta"};
+        
+        
+        final int MAXINTENTS = 7;
+        
+            
+            // Estat gràfic del joc durant la partida
+            char[][] estatPenjat = 
+              new char[estatPenjatInicial.length][estatPenjatInicial[0].length];
+            
+            
+            // Inicialitzar el dibuix del penjat
+            inicialitzarEstatPenjat(estatPenjatInicial,estatPenjat);
+            
+            
+            mostrarEstatPenjat(estatPenjat);
+            
+            // Seleccionar la paraula aleatòriament
+            int index = (int)(Math.random()*paraules.length);
+            String paraula = paraules[index];
+            // Eliminar aquesta línia quan el joc estigui completat
+            paraula = "patata";
+            
+            
+            int totalEncerts = 0,totalErrors = 0;
+            
+            // Estructra de dades (array) per saber quines lletres portem 
+            //encertades            
+            boolean[] lletresEncertades = new boolean[paraula.length()];
+            
+            // Llistat de lletres que hem introduït
+            String lletres = "";
+            
+            do {
+            
+                
+            } while(totalEncerts < paraula.length() && totalErrors < MAXINTENTS);
+        
+    static void mostrarEstatPenjat(char[][] estat) {
+        
+        for (char[] fila : estat) {
+            for (char valor : fila) {
+                System.out.print(valor);
+            }
+            System.out.println("");
+        }
+        
+    }
+    
+    static void inicialitzarEstatPenjat(char[][] estatPenjatIni, char[][] estat) {
+    
+        for (int i = 0; i < estatPenjatIni.length; ++i) {
+            for (int j = 0; j < estatPenjatIni[0].length; ++j) {
+                estat[i][j] = estatPenjatIni[i][j];
+            }
+        }
+        
+    }
+    
+    static void mostrarParaula(String paraula, boolean[] encertades) {
+
+    }
+}
